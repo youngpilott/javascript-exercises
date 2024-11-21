6f75797c07b66e80c39ab951dd4a4a0014d5f5b6
youngpilott <nguyentiendaoduc@cryptopie-labo.com>
2024-11-21 10:25:00 +0700

    done 3rd challenge

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, replicaNumber) {
+    if (replicaNumber < 0) {
+        return "ERROR"
+    }
+    let replicatedWord = "";
+    for (let index = 0; index < replicaNumber; index++) {
+        replicatedWord += word;
+    }
+    return replicatedWord;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedString = ""
+    for (let index = word.length - 1; index >= 0; index--) {
+        reversedString += word[index];
+    }
+    return reversedString
 };
 
+reverseString("hello")
 // Do not edit below this line
 module.exports = reverseString;
